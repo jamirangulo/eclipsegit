590dc90b9fd781372fc1a5ef660fd3fd0200d759
jamir <jamir.an.ba@gmail.com>
2022-04-16 18:24:24 -0500

    se agrega variable m

--- src/demo/ejecucion.java
+++ src/demo/ejecucion.java
@@ -7,11 +7,12 @@ public class ejecucion {
 		int x = 1;
 		int y = 2;
 		int z = 3;
+		int m = 4;
 		
 		System.out.println(x);
 		System.out.println(y);
 		System.out.println(z);
-
+		System.out.println(m);
 	}
 
 }
