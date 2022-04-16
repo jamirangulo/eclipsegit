89add345a448d5c1a4c45df405366c5500d935c6
jamir <jamir.an.ba@gmail.com>
2022-04-16 18:22:24 -0500

    mi primer commit

---
+++ src/demo/ejecucion.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class ejecucion {
+
+	public static void main(String[] args) {
+
+		int x = 1;
+		int y = 2;
+		int z = 3;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+
+	}
+
+}
